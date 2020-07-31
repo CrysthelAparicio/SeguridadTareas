703807c90ba1826e264093c0b1ad5c082d6fa0df
CrysthelAparicio <crysthel.aparicio@unitec.edu>
2020-07-31 17:42:04 -0600

    cambio

--- Monoalfabetico/VersionComando/mono.java
+++ Monoalfabetico/VersionComando/mono.java
@@ -2,19 +2,23 @@ class encript {
   public static String plain = "abcdefghijklmnopqrstuvwxyz";
 
   public static void main(String[] args) {
+    if(args.length == 1 && args[0].equals("help")){
+      help();
+    }
     System.out.println("Text: "+args[1]+"\nKey: "+args[0]+ "\nEncription: "+encripting(validator(args)));
 
   }
 
-  public static String[] validator(String[] args){
-    if(args.length == 1 && args[0].equals("help")){
-      System.out.println("\tBIENVENIDO A ENCRIPT\n\n"
+public static void help(){
+  System.out.println("\tBIENVENIDO A ENCRIPT\n\n"
       + "Para ejecutar encript solo necesita correr el comando >java encript clave texto\n"
       + "\nla clave es la forma de encritacion, debe tener 26 caracteres\n"
       + "el texto es lo que vamos a encriptar\n\n"
-      + "Ejemplo de ejecucion: \njava encript DKVQFIBJWPESCXHTMYAUOLRGZN placeletters");
+      + "Ejemplo de ejecucion: \njava encript DKVQFIBJWPESCXHTMYAUOLRGZN ifwewishtoreplaceletters \n\nEl resultado de la encriptacion anterior es: WIRWAJUHTSDVFSFUUFYA");
       System.exit(0);
-    }
+}
+
+  public static String[] validator(String[] args){
     if(args.length < 2){
       System.err.println("Error! necesita al menos dos parametros para ejecutar la encriptacion. \n más informacion > encript help");
       System.exit(0);
@@ -40,6 +44,7 @@ class encript {
     return encripted;
   }
 }
+
 
 /*
 Paso 1: capturar los argumentos [Listo]
